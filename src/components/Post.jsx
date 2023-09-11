194ebd0114217151d1af16e978655b606c0824ce
William Rodrigues <william.dg88@gmail.com>
2023-09-11 08:40:13 -0300

    Componente Post

---
+++ src/components/Post.jsx
@@ -0,0 +1,30 @@
+import styles from './Post.module.css'
+
+export function Post() {
+    return (
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/56547134?v=4" />
+                    <div className={styles.authorInfo}>
+                        <strong>William Douglas</strong>
+                        <span>Backend Developer</span>
+                    </div>
+                </div>
+
+                <time title='11  de maior às 08:30h' dateTime='2023-05-11 08:30:30'>Publicado há 1h</time>
+            </header>
+
+            <div className={styles.content}><p></p>
+                <p>Fala galeraa 👋</p>
+                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+                <p>👉{' '}<a href=''>jane.design/doctorcare</a></p>
+                <p>
+                    <a href=''>#novoprojeto</a>{' '}
+                    <a href=''>#nlw</a>{' '}
+                    <a href=''>#rocketseat</a>
+                </p>
+            </div>
+        </article>
+    )
+}
