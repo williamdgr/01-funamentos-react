8202b64a961c94819d155327eca6b7c8ce5af5ba
William Rodrigues <william.dg88@gmail.com>
2023-09-20 08:57:05 -0300

    Adicionando propriedades ao component Post

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,37 @@ import styles from './App.module.css';
 
 import './global.css';
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://avatars.githubusercontent.com/u/56547134?v=4',
+      name: 'William Rodrigues',
+      role: 'Backend Engineer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' }
+    ],
+    publishedAt: new Date('2023-09-18 08:00:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://avatars.githubusercontent.com/u/56547134?v=4',
+      name: 'William Rodrigues 2',
+      role: 'Backend Engineer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' }
+    ],
+    publishedAt: new Date('2023-09-10 09:00:00')
+  }
+];
+
 
 export function App() {
 
@@ -17,12 +48,17 @@ export function App() {
         <Sidebar />
 
         <main>
-          <Post author="William Douglas" content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. " />
-          <Post author="Débora Caroline" content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. "/>
+          {posts.map(post => {
+              return(
+                <Post 
+                  author={post.author}
+                  content={post.content}
+                  publishedAt={post.publishedAt}
+                />
+              )
+          })}
         </main>
-
       </div>
-
     </div>
   )
 }
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,31 +1,42 @@
 import { Avatar } from './Avatar'
 import { Comment } from './Comment'
 import styles from './Post.module.css'
+import { format, formatDistanceToNow } from "date-fns";
+import ptBR from "date-fns/locale/pt-BR";
 
-export function Post() {
+export function Post({ author, publishedAt, content }) {
+
+    const publishedDateFormatted = format(publishedAt, "dd 'de' LLLL 'às' HH:mm'h'", {
+        locale : ptBR,
+    })
+    
+    const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+        locale: ptBR,
+        addSuffix: true,
+    })
+
     return (
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <Avatar src="https://avatars.githubusercontent.com/u/56547134?v=4" />
+                    <Avatar src={author.avatarUrl} />
                     <div className={styles.authorInfo}>
-                        <strong>William Douglas</strong>
+                        <strong>{author.name}</strong>
                         <span>Backend Developer</span>
                     </div>
                 </div>
 
-                <time title='11  de maior às 08:30h' dateTime='2023-05-11 08:30:30'>Publicado há 1h</time>
+                <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>{publishedDateRelativeToNow}</time>
             </header>
 
             <div className={styles.content}><p></p>
-                <p>Fala galeraa 👋</p>
-                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-                <p>👉{' '}<a href=''>jane.design/doctorcare</a></p>
-                <p>
-                    <a href=''>#novoprojeto</a>{' '}
-                    <a href=''>#nlw</a>{' '}
-                    <a href=''>#rocketseat</a>
-                </p>
+                {content.map(line => {
+                       if (line.type === 'paragraph') {
+                        return <p>{line.content}</p>
+                       } else if (line.type === 'link') {
+                        return <p><a href='#'>{line.content}</a></p>
+                       } 
+                })}
             </div>
 
             <form className={styles.commentForm}>
